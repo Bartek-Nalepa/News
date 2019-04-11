507c9fafd2a0ff9ce486ac147b25da9ab3ffcefa
Bartek-Nalepa <nalepabartek95@gmail.com>
2019-04-11 20:29:20 +0200

    RN

--- src/Screens.js
+++ src/Screens.js
@@ -1,7 +1,6 @@
 import {Navigation} from "react-native-navigation";
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 import Main from "./Main";
 
 export function registerScreens() {
-    Navigation.registerComponent('Main', () => gestureHandlerRootHOC(Main));
+    Navigation.registerComponent('Main', () => Main);
 }
